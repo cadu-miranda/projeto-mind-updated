23f89113e27024cf4e578ea38735a0fbfa6d98cf
Carlos Miranda <polomiranda@outlook.com>
2021-07-11 03:33:46 -0300

    main

--- frontend/src/components/UserTable/UserTable.jsx
+++ frontend/src/components/UserTable/UserTable.jsx
@@ -59,7 +59,7 @@ const UserTable = () => {
                   }}
                   className="button"
                   variant="contained"
-                  color="primary"
+                  color="error"
                   href="#contained-buttons"
                 >
                   Excluir
