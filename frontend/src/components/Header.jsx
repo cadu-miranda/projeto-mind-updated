afafc370c3984331a75850eddfa7e7d3af219657
Carlos Miranda <polomiranda@outlook.com>
2021-07-11 01:54:15 -0300

    main

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <>
       <header className="header">
         <h1>
-          <a href="/"> Projeto - Mind</a>
+          <a href="/"> Projeto - Mind Consulting</a>
         </h1>
       </header>
     </>
